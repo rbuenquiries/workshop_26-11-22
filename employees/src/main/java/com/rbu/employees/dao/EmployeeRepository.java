c664d06356d2fbb6acf58554753a7a1426c52c97
rbuenquiries <118588722+rbuenquiries@users.noreply.github.com>
2022-11-27 20:04:53 +0530

    Add files via upload

---
+++ employees/src/main/java/com/rbu/employees/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.rbu.employees.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rbu.employees.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+
+}
